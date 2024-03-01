e4049f013ef852229bfc8d0ec6afbcf63001726d
Egeschalt <eberlincesar84@gmail.com>
2024-03-01 10:28:59 -0600

    Avanze v3 creacion de las actividades faltantes

---
+++ app/src/main/java/com/example/appbibliotecaeapis/Bienvenido.java
@@ -0,0 +1,13 @@
+package com.example.appbibliotecaeapis;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bienvenido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenido);
+    }
+}
