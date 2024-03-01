84286e05dba87371e3cf85b4b40ba191261d7716
Egeschalt <eberlincesar84@gmail.com>
2024-02-29 18:36:11 -0600

    Avanze v1 primera pantalla

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.appbibliotecaeapis"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.appbibliotecaeapis"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
