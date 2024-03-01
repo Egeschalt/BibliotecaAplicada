de3b88cc6e60a1496651d72eb3dd85404f1385c8
Egeschalt <eberlincesar84@gmail.com>
2024-02-29 23:28:17 -0600

    Avanze v2 Segunda y tercera pantalla

--- app/src/main/java/com/example/appbibliotecaeapis/IniciarSesion.java
+++ app/src/main/java/com/example/appbibliotecaeapis/IniciarSesion.java
@@ -5,23 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.*;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class IniciarSesion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void PantallaSesion(View view){
-        Context context=view.getContext();
-        Intent intent=new Intent(context, IniciarSesion.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_iniciar_sesion);
     }
     public void PantallaRegistro(View view){
         Context context=view.getContext();
         Intent intent=new Intent(context, Registrar.class);
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/example/appbibliotecaeapis/Registrar.java
+++ app/src/main/java/com/example/appbibliotecaeapis/Registrar.java
@@ -5,23 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.*;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Registrar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registrar);
     }
     public void PantallaSesion(View view){
         Context context=view.getContext();
         Intent intent=new Intent(context, IniciarSesion.class);
-        startActivity(intent);
-    }
-    public void PantallaRegistro(View view){
-        Context context=view.getContext();
-        Intent intent=new Intent(context, Registrar.class);
         startActivity(intent);
     }
 }
