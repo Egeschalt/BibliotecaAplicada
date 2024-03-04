2840ecc880121f9e83a51d0f042e447b09ebbb08
USER <varassjm@gmail.com>
2024-03-03 21:38:41 -0500

    VMBibliotecario V6

--- app/src/main/java/VistaModelo/VMBibliotecario.java
+++ app/src/main/java/VistaModelo/VMBibliotecario.java
@@ -1,5 +1,46 @@
 package VistaModelo;
 
-public class VMBibliotecario {
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
+import java.util.ArrayList;
+
+import Model.Bibliotecario;
+
+public class VMBibliotecario {
+    ArrayList<Bibliotecario> listaBibliotecarios;
+    String nombreBD;
+    Activity oActivity;
+    int version;
+
+    public VMBibliotecario(Activity oActivity) {
+        listaBibliotecarios=new ArrayList<>();
+        nombreBD="BDBiblioteca";
+        version=1;
+        this.oActivity=oActivity;
+        InsertarBibliotecario();
+    }
+
+    private void InsertarBibliotecario(){
+
+    }
+    private boolean AgregarBibliotecario(Bibliotecario bibliotecario) {
+        boolean rpta = false;
+        BDPrestamoOpenHelper bdBibliotecarioOpenHelper = new BDPrestamoOpenHelper(oActivity, nombreBD, null, version);
+        SQLiteDatabase oBD = bdBibliotecarioOpenHelper.getWritableDatabase();
+        if (oBD != null) {
+            ContentValues oRegistro = new ContentValues();
+            oRegistro.put("IdBibliotecario", bibliotecario.getIdBibliotecario());
+            oRegistro.put("nombreBibliotecario", bibliotecario.getNombre());
+            long fila = oBD.insert("Bibliotecario", null, oRegistro);
+            Log.d("valor de fila", String.valueOf(fila));
+            if (fila > 1) {
+                rpta = true;
+                oBD.close();
+            }
+        }
+        return rpta;
+    }
 }
