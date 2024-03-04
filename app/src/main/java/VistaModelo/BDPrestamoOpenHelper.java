abe12ec133e708cedec0fccf25332d6580211851
DIEGO <dhoyosc21_1@unc.edu.pe>
2024-03-03 21:24:37 -0500

    Creación de la base de datos y tabla Estudiante.v6

---
+++ app/src/main/java/VistaModelo/BDPrestamoOpenHelper.java
@@ -0,0 +1,35 @@
+package VistaModelo;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class BDPrestamoOpenHelper extends SQLiteOpenHelper {
+    String tabla_estudiante = "CREATE TABLE Estudiante(" +
+            "IdEstudiante INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+            "Nombre VARCHAR(40) NOT NULL UNIQUE," +
+            "Apellidos VARCHAR(40) NOT NULL," +
+            "Dni VARCHAR(8) NOT NULL," +
+            "Carrera VARCHAR(40) NOT NULL," +
+            "Correo VARCHAR(40) NOT NULL," +
+            "Teléfono VARCHAR(9) NOT NULL," +
+            "descripcion TEXT," +
+            "imagen BYTE)";
+
+
+    public BDPrestamoOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(tabla_estudiante);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS Estudiante");
+        onCreate(db);
+    }
+}
--- app/src/main/java/VistaModelo/VMEstudiante.java
+++ app/src/main/java/VistaModelo/VMEstudiante.java
@@ -1,5 +1,53 @@
 package VistaModelo;
 
-public class VMEstudiante {
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
+import java.util.ArrayList;
+
+import Model.Estudiante;
+
+public class VMEstudiante {
+    ArrayList<Estudiante> listaEstudiantes;
+    String nombreBD;
+    Activity oActivity;
+    int version;
+
+    public VMEstudiante(Activity oActivity) {
+        listaEstudiantes=new ArrayList<>();
+        nombreBD="BDBiblioteca";
+        version=1;
+        this.oActivity=oActivity;
+        InsertarEstudiantes();
+    }
+
+    private void InsertarEstudiantes(){
+      //  Estudiante est1=new Estudiante("01","Maycol","Varas","72346482","Ingeniería de Sistemas","jvaras","a",);
+       // Estudiante est2=new Estudiante("02","Diego");
+//        Estudiante est3=new Estudiante("03","Jhosmer");
+//        Estudiante est4=new Estudiante("04","Hans");
+//        Estudiante est5=new Estudiante("05","Ever");
+//        AgregarEstudiante(est1);
+
+
+    }
+    private boolean AgregarEstudiante(Estudiante estudiante) {
+        boolean rpta = false;
+        BDPrestamoOpenHelper bdEstudiantesOpenHelper = new BDPrestamoOpenHelper(oActivity, nombreBD, null, version);
+        SQLiteDatabase oBD = bdEstudiantesOpenHelper.getWritableDatabase();
+        if (oBD != null) {
+            ContentValues oRegistro = new ContentValues();
+            oRegistro.put("IdEstudiante", estudiante.getCodEstudiante());
+            oRegistro.put("nombreEstudiante", estudiante.getNombre());
+            long fila = oBD.insert("Estudiante", null, oRegistro);
+            Log.d("valor de fila", String.valueOf(fila));
+            if (fila > 1) {
+                rpta = true;
+                oBD.close();
+            }
+        }
+        return rpta;
+    }
 }
