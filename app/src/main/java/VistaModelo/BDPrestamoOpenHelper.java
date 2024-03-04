9ba153d69fbaf5d8506522fa7b5b767df0e04dfe
DIEGO <dhoyosc21_1@unc.edu.pe>
2024-03-03 21:39:07 -0500

    Creación tabla Bibliotecario.v6.1

--- app/src/main/java/VistaModelo/BDPrestamoOpenHelper.java
+++ app/src/main/java/VistaModelo/BDPrestamoOpenHelper.java
@@ -5,6 +5,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.Date;
+
 public class BDPrestamoOpenHelper extends SQLiteOpenHelper {
     String tabla_estudiante = "CREATE TABLE Estudiante(" +
             "IdEstudiante INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
@@ -14,9 +16,26 @@ public class BDPrestamoOpenHelper extends SQLiteOpenHelper {
             "Carrera VARCHAR(40) NOT NULL," +
             "Correo VARCHAR(40) NOT NULL," +
             "Teléfono VARCHAR(9) NOT NULL," +
-            "descripcion TEXT," +
-            "imagen BYTE)";
+            "Imagen BYTE)";
 
+    String tabla_bibliotecario = "CREATE TABLE Bibliotecario(" +
+            "IdBibliotecario INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+            "Nombre VARCHAR(40) NOT NULL UNIQUE," +
+            "Apellidos VARCHAR(40) NOT NULL," +
+            "Teléfono VARCHAR(9) NOT NULL," +
+            "descripcion TEXT," +
+            "Imagen BYTE)";
+
+    String tabla_libro = "CREATE TABLE Libro(" +
+            "IdLibro INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+            "Título VARCHAR(40) NOT NULL UNIQUE," +
+            "Autor VARCHAR(40) NOT NULL," +
+            "Editorial VARCHAR(40) NOT NULL," +
+            "Género VARCHAR(20) NOT NULL," +
+            "Idioma VARCHAR(20) NOT NULL," +
+            "Fecha de publicación VARCHAR(20) NOT NULL," +
+            "Disponibilidad BOOLEAN NOT NULL," +
+            "Imagen BYTE)";
 
     public BDPrestamoOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
