8b8ea0b893432d09c165f0ab87319780b0e58777
Hans <egracianoh211_1@unc.edu.pe>
2024-03-05 20:31:22 -0500

    Agregué la actividad "Agregar Libro"

---
+++ app/src/main/java/com/example/appbibliotecaeapis/AgregarLibroActivity.java
@@ -0,0 +1,14 @@
+package com.example.appbibliotecaeapis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarLibroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_libro);
+    }
+}
