bda1603b778003fd1d8f6028dffd525c9558b58a
Hans <egracianoh211_1@unc.edu.pe>
2024-03-08 15:40:11 -0500

    Agregué las actividades para modificar, eliminar y listar libros, además de los IDs para cada widget que se va a utilizar, en caso de que falte alguno, por favor agreguenlo.

---
+++ app/src/main/java/com/example/appbibliotecaeapis/EliminarLibroActivity.java
@@ -0,0 +1,14 @@
+package com.example.appbibliotecaeapis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EliminarLibroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eliminar_libro);
+    }
+}
---
+++ app/src/main/java/com/example/appbibliotecaeapis/ModificarLibro1_2_Activity.java
@@ -0,0 +1,14 @@
+package com.example.appbibliotecaeapis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModificarLibro1_2_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_libro12);
+    }
+}
